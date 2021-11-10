e4ee808ccdfdfddaa2b0e0dd7db57262b8ac7503
jflash <jflash@mail.ee>
2021-11-10 10:41:52 +0200

    csseditor.php - codemirror editor
    Update for Joomla 4. One change in cssconfig.php file.

--- elements/csseditor.php
+++ elements/csseditor.php
@@ -35,7 +35,11 @@ class JFormFieldCSSEditor extends JFormField {
 		if(!JPluginHelper::isEnabled('editors', 'codemirror')) return '<strong>Sorry: this plugin needs the CodeMirror editor plug-in to be enabled</strong>';
 		$app = JFactory::getApplication('administrator');
 		$app->setUserState('editor.source.syntax', 'css');
-		$editor = JFactory::getEditor('codemirror');
+		if(JVERSION<4){
+			$editor = JFactory::getEditor('codemirror');
+		} else {
+			$editor = JEditor::getInstance('codemirror');
+		}
 		$params = array(
 					'linenumbers'=> '1' ,
                  'tabmode'  => 'shift'
